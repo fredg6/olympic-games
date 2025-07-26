633f67b12ba523d9b8727a173365e3efd9dbcd1f
Fred <freedgermain@gmail.com>
2025-07-26 14:05:11 +0200

    Display first chart

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,19 +1,17 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Olympic } from '../../core/models/Olympic';
 import { OlympicService } from '../../core/services/olympic.service';
+import { PieChart } from '../../shared/charts/pie-chart/pie-chart';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    AsyncPipe
-  ],
+  imports: [PieChart],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
 export class Home implements OnInit {
-  public olympics$!: Observable<Olympic[]>;
+  olympics$!: Observable<Olympic[]>;
 
   private olympicService = inject(OlympicService);
 
