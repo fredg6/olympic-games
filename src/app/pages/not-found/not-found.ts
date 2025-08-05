839920b5cc2d760a46655b548341bded601965f3
Fred <freedgermain@gmail.com>
2025-08-05 15:32:05 +0200

    Import RouterLink in NotFound component

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
+  imports: [
+    RouterLink
+  ],
   templateUrl: './not-found.html',
   styleUrls: ['./not-found.scss']
 })
